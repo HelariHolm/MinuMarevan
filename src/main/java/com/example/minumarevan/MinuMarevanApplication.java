a8d6a6575ee36284503ff457b9c84746233d78e0
HelariHolm <ruun@hot.ee>
2021-11-20 21:24:29 +0200

    Initial commit, project created

---
+++ src/main/java/com/example/minumarevan/MinuMarevanApplication.java
@@ -0,0 +1,13 @@
+package com.example.minumarevan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinuMarevanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinuMarevanApplication.class, args);
+    }
+
+}
