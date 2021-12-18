49a686f5ce012a9d42c392043badc6cedc09bdea
HelariHolm <ruun@hot.ee>
2021-12-18 23:31:53 +0200

    BugFix: Updated features home.html, UserController and ContactNumbers for correct My Profile edit function which previously caused data loss.

--- src/main/java/com/example/minumarevan/model/ContactNumbers.java
+++ src/main/java/com/example/minumarevan/model/ContactNumbers.java
@@ -11,7 +11,6 @@ import javax.persistence.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
 public class ContactNumbers {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
